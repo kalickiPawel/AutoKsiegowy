86760c705e534e0a29a722749cfed26ce6586453
Pawel Kalicki <kalicki.paul@icloud.com>
2018-12-18 03:57:01 +0100

    Added basic functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Wydatek, KATEGORIE } from './wydatek';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AutoKsiegowy';
+  wydatki: Wydatek[];
+  ngOnInit(){
+    this.wydatki = [
+      new Wydatek('Tankowanie', 252.43, '', new Date(2018, 4, 30)),
+      new Wydatek('Myjnia', 75, '', new Date(2018, 4, 2)),
+      new Wydatek('Serwis', 1250, 'Klocki, rozrząd i parę drobiazgów', new Date(2018, 3, 16)),
+      new Wydatek('Inne', 315.50, 'Różowe futerko na kierownicę', new Date(2018, 3, 16)),
+      new Wydatek('Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12))
+    ];
+  }
 }
---
+++ src/app/wydatek.ts
@@ -0,0 +1,10 @@
+export class Wydatek {
+    constructor(
+        public kategoria: string,
+        public kwota: number,
+        public opis: string,
+        public data: Date){
+    }
+}
+
+export const KATEGORIE = [ 'Tankowanie', 'Myjnia', 'Serwis', 'Inne' ];
