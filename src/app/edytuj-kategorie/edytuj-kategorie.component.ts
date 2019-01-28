dcceba694519ee6cd18bc0236bb52151a5246735
Pawel Kalicki <kalicki.paul@icloud.com>
2019-01-28 15:10:21 +0100

    Added add/remove function

--- src/app/edytuj-kategorie/edytuj-kategorie.component.ts
+++ src/app/edytuj-kategorie/edytuj-kategorie.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { WydatkiService } from './../wydatki.service';
 import { Router } from '@angular/router';
+import { Wydatek, KATEGORIE } from './../wydatek';
 
 @Component({
   selector: 'app-edytuj-kategorie',
   templateUrl: './edytuj-kategorie.component.html',
   styleUrls: ['./edytuj-kategorie.component.css']
 })
+
 export class EdytujKategorieComponent implements OnInit {
 
   kategorie: string[];
@@ -16,5 +18,11 @@ export class EdytujKategorieComponent implements OnInit {
   ngOnInit() {
     this.kategorie = this.wydatkiService.getKategorie();
   }
-
+  dodajKategorie(kategoria: string) {
+    KATEGORIE.push(kategoria);
+  }
+  usunKategorie(kategoria: string) {
+    KATEGORIE.filter(function(ele) { return ele !== kategoria;
+    });
+  }
 }
